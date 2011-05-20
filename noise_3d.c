9932da5ae302b230b8cf3b3eca8340d7a2efc713
Robin Redeker <elmex@ta-sa.org>
2011-05-20 18:03:11 +0200

    wrote generic drawing library for the volumes.

---
+++ noise_3d.c
@@ -0,0 +1,133 @@
+#define INTSCALE  (128ul)
+#define INTSCALE3 (INTSCALE * INTSCALE * INTSCALE)
+
+unsigned int rnd_xor (unsigned int x)
+{
+   x ^= (x << 6);
+   x ^= (x >> 3);
+   x ^= (x << 17);
+   return x;
+}
+
+unsigned int hash32int (unsigned int i)
+{
+  i = (i << 15) - i - 1;
+  i = i ^ (i >> 12);
+  i = i + (i << 2);
+  i = i ^ (i >> 4);
+  i = i * 2057;         // key = (key + (key << 3)) + (key << 11);
+  i = i ^ (i >> 16);
+  return i;
+}
+
+unsigned long map_coord2int (unsigned int x, unsigned int y, unsigned int z, unsigned int seed)
+{
+  unsigned long out = 0;
+  unsigned int i = 0;
+  while (x > 0 || y > 0 || z > 0)
+    {
+      out += (x & 0x1) ? (0x1 << i) : 0;
+      i++;
+      out += (y & 0x1) ? (0x1 << i) : 0;
+      i++;
+      out += (z & 0x1) ? (0x1 << i) : 0;
+      i++;
+      x >>= 1;
+      y >>= 1;
+      z >>= 1;
+    }
+  return hash32int (hash32int (out) + seed);
+}
+
+#define NOISE_ARR_OFFS(slen,x,y,z) (1 + (x) + (y) * slen + (z) * (slen * slen))
+
+void *mk_3d_noise (unsigned int slen, unsigned int seed)
+{
+   int x, y, z;
+
+   slen++; // sample one more at the edge
+
+   unsigned long *noise_arr =
+      malloc (sizeof (unsigned long) * (slen * slen * slen + 1));
+
+   noise_arr[0] = slen;
+
+   for (x = 0; x < slen; x++)
+     for (y = 0; y < slen; y++)
+       for (z = 0; z < slen; z++)
+         seed = noise_arr[NOISE_ARR_OFFS(slen,x,y,z)] = rnd_xor (seed);
+
+   return noise_arr;
+}
+
+unsigned int linerp_int (unsigned int a, unsigned int b, unsigned int x)
+{
+   unsigned long i =
+        ((unsigned long) a * (INTSCALE - x)) + ((unsigned long) b * x);
+   i /= INTSCALE;
+   return i;
+}
+
+unsigned int smoothstep_int (unsigned int a, unsigned int b, unsigned int x)
+{
+   unsigned long xs = x;
+   xs = xs * xs * ((3 * INTSCALE) - 2 * xs);
+   unsigned long i =
+          ((unsigned long) a * (INTSCALE3 - xs))
+        + ((unsigned long) b * xs);
+   i /= INTSCALE3;
+   return i;
+}
+
+unsigned long sample_3d_noise_at (void *noise, unsigned int x, unsigned int y, unsigned int z, unsigned int scale)
+{
+   unsigned long *noise_3d = noise;
+
+   // reduces the sampling scale, higher "scale" means less
+   // points from noise are looked at and more interpolation
+
+   unsigned int x_rest = (INTSCALE * (x % scale)) / scale;
+   x /= scale;
+   unsigned int y_rest = (INTSCALE * (y % scale)) / scale;
+   y /= scale;
+   unsigned int z_rest = (INTSCALE * (z % scale)) / scale;
+   z /= scale;
+
+   //d// printf ("red %d %d %d, rest %ud %ud %ud %ud\n", x, y, z, scale, x_rest, y_rest, z_rest);
+   unsigned int slen = noise_3d[0];
+
+   if ((z + 1) >= slen
+       || (y + 1) >= slen
+       || (x + 1) >= slen)
+     return 0;
+
+   unsigned int samples[8];
+   samples[0] = noise_3d[NOISE_ARR_OFFS(slen,x    , y,     z)];
+   samples[1] = noise_3d[NOISE_ARR_OFFS(slen,x + 1, y,     z)];
+
+   samples[2] = noise_3d[NOISE_ARR_OFFS(slen,x    , y + 1, z)];
+   samples[3] = noise_3d[NOISE_ARR_OFFS(slen,x + 1, y + 1, z)];
+
+   samples[4] = noise_3d[NOISE_ARR_OFFS(slen,x    , y,     z + 1)];
+   samples[5] = noise_3d[NOISE_ARR_OFFS(slen,x + 1, y,     z + 1)];
+
+   samples[6] = noise_3d[NOISE_ARR_OFFS(slen,x    , y + 1, z + 1)];
+   samples[7] = noise_3d[NOISE_ARR_OFFS(slen,x + 1, y + 1, z + 1)];
+
+   samples[0] = smoothstep_int (samples[0], samples[1], x_rest);
+   samples[1] = smoothstep_int (samples[2], samples[3], x_rest);
+   samples[2] = smoothstep_int (samples[4], samples[5], x_rest);
+   samples[3] = smoothstep_int (samples[6], samples[7], x_rest);
+
+   samples[0] = smoothstep_int (samples[0], samples[1], y_rest);
+   samples[1] = smoothstep_int (samples[2], samples[3], y_rest);
+
+   return smoothstep_int (samples[0], samples[1], z_rest);
+}
+
+void free_3d_noise (void *noise)
+{
+  free (noise);
+}
+
+
---
+++ volume_draw.c
@@ -0,0 +1,205 @@
+#include <math.h>
+#include "noise_3d.c"
+
+typedef struct _vol_draw_ctx {
+  unsigned int size;
+  double      *src;
+  double      *dst;
+
+  unsigned int draw_op;
+
+#define VOL_DRAW_ADD 1
+#define VOL_DRAW_SUB 2
+#define VOL_DRAW_MUL 3
+#define VOL_DRAW_SET 4
+
+} vol_draw_ctx;
+
+#define DRAW_DST(x,y,z) DRAW_CTX.dst[(x) + (y) * DRAW_CTX.size + (z) * (DRAW_CTX.size * DRAW_CTX.size)]
+
+// two buffers:
+//    source
+//    dest
+//
+// Draw Styles:
+//   - cantor dust
+//   - menger sponge
+//   - sphere-fractal-like: 2 styles: value range fill, empty
+//   - random spheres: also 2 styles
+//
+// Fill Styles:
+//   - noise octaves with parameters
+//   - constant fill from [0,1]
+//   - draw board source (operation: swap dest/source buffer)
+//
+// Draw Ops:
+//   - add
+//   - sub
+//   - mul
+//   - set
+
+static vol_draw_ctx DRAW_CTX;
+
+void vol_draw_init ()
+{
+  DRAW_CTX.src  = 0;
+  DRAW_CTX.dst  = 0;
+  DRAW_CTX.size = 0;
+  DRAW_CTX.draw_op = 0;
+}
+
+void vol_draw_alloc (unsigned int size)
+{
+  if (DRAW_CTX.src)
+    {
+      free (DRAW_CTX.src);
+      free (DRAW_CTX.dst);
+    }
+
+  DRAW_CTX.src = malloc (sizeof (double) * size * size * size);
+  DRAW_CTX.dst = malloc (sizeof (double) * size * size * size);
+  memset (DRAW_CTX.src, 0, sizeof (double) * size * size * size);
+  memset (DRAW_CTX.dst, 0, sizeof (double) * size * size * size);
+  DRAW_CTX.size = size;
+}
+
+void vol_draw_set_op (unsigned int op)
+{
+  DRAW_CTX.draw_op = op;
+}
+
+void vol_draw_swap ()
+{
+  double *s = DRAW_CTX.src;
+  DRAW_CTX.src = DRAW_CTX.dst;
+  DRAW_CTX.dst = s;
+}
+
+void vol_draw_op (unsigned int x, unsigned int y, unsigned int z, double val)
+{
+  switch (DRAW_CTX.draw_op)
+    {
+      case VOL_DRAW_ADD:
+        DRAW_DST(x,y,z) += val;
+        break;
+
+      case VOL_DRAW_SUB:
+        DRAW_DST(x,y,z) -= val;
+        if (DRAW_DST(x,y,z) < 0)
+          DRAW_DST(x,y,z) = 0;
+        break;
+
+      case VOL_DRAW_MUL: DRAW_DST(x,y,z) *= val; break;
+      case VOL_DRAW_SET: DRAW_DST(x,y,z) = val; break;
+    }
+}
+
+void vol_fill_simple_noise_octaves (unsigned int seed, unsigned int octaves, double factor, double persistence)
+{
+  double amp_correction = 0;
+
+  if (seed == 0) seed = 1;
+
+
+  void *noise = mk_3d_noise (DRAW_CTX.size, seed);
+
+  int i;
+  for (i = 0; i <= octaves; i++)
+    {
+      double scale = pow (factor, octaves - i);
+      double amp   = pow (persistence , i);
+      amp_correction += amp;
+
+      printf ("NOISE %d %g %g\n", i, scale, amp);
+      int x, y, z;
+      for (z = 0; z < DRAW_CTX.size; z++)
+        for (y = 0; y < DRAW_CTX.size; y++)
+          for (x = 0; x < DRAW_CTX.size; x++)
+            {
+              unsigned long s = sample_3d_noise_at (noise, x, y, z, scale);
+              double val = (double) s / (double) 0xFFFFFFFF;
+              DRAW_DST(x,y,z) += val * amp;
+            }
+    }
+
+  free_3d_noise (noise);
+
+  int x, y, z;
+  for (z = 0; z < DRAW_CTX.size; z++)
+    for (y = 0; y < DRAW_CTX.size; y++)
+      for (x = 0; x < DRAW_CTX.size; x++)
+        DRAW_DST(x,y,z) /= amp_correction;
+}
+
+//void draw_model_menger_sponge_box (float x, float y, float z, float size, int max, int lvl)
+//{
+//  if (lvl == 0)
+//    {
+//        int j, k, l;
+//     for (j = 0; j < size; j++)
+//       for (k = 0; k < size; k++)
+//         for (l = 0; l < size; l++)
+//          model[OFFS((unsigned int) x + j, (unsigned int) y + k,(unsigned int)  z + l)] = 1;
+//      return;
+//    }
+//
+//   float j, k, l;
+//   float s3 = size / 3;
+//   for (j = 0; j < 3; j++)
+//     for (k = 0; k < 3; k++)
+//       for (l = 0; l < 3; l++)
+//         {
+//           int cnt_max = 0;
+//           if (j == 0 || j == 2)
+//             cnt_max++;
+//           if (k == 0 || k == 2)
+//             cnt_max++;
+//           if (l == 0 || l == 2)
+//             cnt_max++;
+//
+//           if (cnt_max < 2)
+//             continue;
+//
+//           draw_model_menger_sponge_box (
+//             x + j * s3, y + k * s3, z + l * s3, s3, max, lvl - 1);
+//         }
+//}
+//
+//
+//void draw_model_cantor_dust_box (float x, float y, float z, float size, int max, int lvl)
+//{
+//  if (lvl == 0)
+//    {
+//        int j, k, l;
+//     for (j = 0; j < size; j++)
+//       for (k = 0; k < size; k++)
+//         for (l = 0; l < size; l++)
+//           {
+//            int xi = x + j, yi = y + k, zi = z + l;
+//             if (xi >= max || yi >= max || zi >= max)
+//               return;
+//            model[OFFS(xi, yi, zi)] = 1;
+//           }
+//      return;
+//    }
+//
+//   float rad = (float) lvl / 1.3;
+//   rad = rad < 1 ? 1 : rad;
+//
+//   size /= 2;
+//   size -= rad;
+//
+//   float offs = size + 2 * rad;
+//
+//   draw_model_cantor_dust_box (x,        y,        z,        size, max, lvl - 1);
+//   draw_model_cantor_dust_box (x + offs, y,        z,        size, max, lvl - 1);
+//   draw_model_cantor_dust_box (x       , y,        z + offs, size, max, lvl - 1);
+//   draw_model_cantor_dust_box (x + offs, y,        z + offs, size, max, lvl - 1);
+//
+//   draw_model_cantor_dust_box (x,        y + offs, z,        size, max, lvl - 1);
+//   draw_model_cantor_dust_box (x + offs, y + offs, z,        size, max, lvl - 1);
+//   draw_model_cantor_dust_box (x       , y + offs, z + offs, size, max, lvl - 1);
+//   draw_model_cantor_dust_box (x + offs, y + offs, z + offs, size, max, lvl - 1);
+//}
+//
+//
