4c57576a84047f278fb7e1c9568c6fe30d4bac30
Robin Redeker <elmex@ta-sa.org>
2011-06-27 23:19:29 +0200

    some bump up.

--- world.c
+++ world.c
@@ -6,11 +6,9 @@
 
 #define CHUNK_SIZE      12
 #define CHUNKS_P_SECTOR  5
-#define MAX_LIGHT_RADIUS 12
+#define MAX_LIGHT_RADIUS 18
 #define MAX_LIGHT_RADIUS_CHUNKS ( 6 * ((MAX_LIGHT_RADIUS + 1) * 2) * ((MAX_LIGHT_RADIUS + 1) * 2) * ((MAX_LIGHT_RADIUS + 1) * 2))
 // => 26^3 * 6 neighbors => ~1.3Mb ringbuffer for queue - should be enough :)
-
-
 
 #define CHUNK_ALEN (CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE)
 #define POSSIBLE_OBJECTS 4096 // this is the max number of different object types!
